7daea1f690a584dcdca72674f98c032b278a492a
Juan Pablo Zapata <juan.zapata@dfx5.com>
2025-07-14 20:53:19 -0500

    change apprunner file

--- app/main.py
+++ app/main.py
@@ -1,23 +1,7 @@
-from fastapi import FastAPI, Request, Response, HTTPException
-import os
+from fastapi import FastAPI
 
-#APP_ID = os.getenv("X_APPLICATION_ID")        
-APP_ID = "X_APPLICATION_ID"
+app = FastAPI()
 
-if not APP_ID:
-    raise RuntimeError("Falta X_APPLICATION_ID")
-
-app = FastAPI(root_path="/api/rappi-connect-dev")  
-HDR = "x_application_id"
-
-@app.middleware("http")
-async def require_app_id(request: Request, call_next):
-    if request.headers.get(HDR, APP_ID) != APP_ID:
-        raise HTTPException(400, f"Valor inválido de {HDR}")
-    resp: Response = await call_next(request)
-    resp.headers[HDR] = APP_ID
-    return resp
-
-@app.get("/health")                     
-async def health():
-    return {"status": "ok"}
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
